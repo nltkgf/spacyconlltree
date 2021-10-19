1a1c8118f8c2950b03a5a736151e6450635647d7
Meowyam <maryam@chilon.net>
2021-10-19 23:03:49 +0800

    get functions

--- treefrom.py
+++ treefrom.py
@@ -15,13 +15,29 @@ def getTree(text):
     Tree = {}
     # print(token.text, token.lemma_, token.pos_, token.dep_, token.head.text)
     if token.dep_.lower() == 'root':
-      Tree['root'] = [token.text, token.lemma_, token.dep_, token]
+      Tree['root'] = [token.text, token.lemma_, token.dep_.lower(), token]
       unfiltered = [[child.text, child.lemma_, child.dep_, child] for child in token.children]
       Tree['children'] = removePunct(unfiltered)
       #print(Tree)
       trees.append(Tree)
       return trees
       #print(trees)
+
+# def funElements(trees):
+#   for tree in trees:
+#       fun_elements = []
+#       for child in tree['children']:
+#         fun_elements.append(child[2])
+#         return fun_elements
+
+def writeFun(trees):
+  for tree in trees:
+    fun_elements = []
+    for child in tree['children']:
+      fun_elements.append(child[2])
+      fun_name = '_'.join(fun_elements)
+      fun = fun_name + " : " + ' -> '.join([tree['root'][2]] + fun_elements) + ' -> UDS'
+      print(fun)
 
 with open(filename) as input:
   for _ in range(4):
@@ -32,4 +48,4 @@ with open(filename) as input:
     # doc = nlp(text)
 
     allTrees = getTree(text)
-    print(allTrees)
+    writeFun(allTrees)
